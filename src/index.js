0496ef833c9271fa886491db6e9c046347741ce7
jacky32 <patrki@atlas.cz>
2022-10-03 16:33:02 +0200

    Add future weather dom controller

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const city = "Ostrava";
 const submitCity = async () => {
   const geolocation = await getGeolocation(citySelect.value);
   const weatherData = await getWeatherData(geolocation.lat, geolocation.lon);
-  addToDOM(
+  addTodayWeatherData(
     geolocation.name,
     geolocation.state,
     weatherData.temp,
@@ -39,7 +39,7 @@ const getWeatherData = async (lat, lon) => {
 };
 
 // updates weather data in DOM
-const addToDOM = (city, state, temp, wind, humidity, iconData) => {
+const addTodayWeatherData = (city, state, temp, wind, humidity, iconData) => {
   const outputCity = document.getElementById("output-city");
   const outputState = document.getElementById("output-state");
   const outputTemp = document.getElementById("output-temp");
@@ -56,6 +56,30 @@ const addToDOM = (city, state, temp, wind, humidity, iconData) => {
   outputHumidity.textContent = humidity;
   outputIcon.innerHTML = icon;
 };
+
+const addFutureWeatherData = (dayName, dayTemp, dayHumid, dayWind, dayIcon) => {
+  const days = document.getElementsByClassName("day");
+  for (let i = 0; i < days.length; i++) {
+    days[i].childNodes.forEach((item) => {
+      if (item.nodeType == 3) return;
+      if (item.classList.contains("day-name")) {
+        item.textContent = dayName;
+      } else if (item.classList.contains("day-temp")) {
+        item.textContent = dayTemp + " °C";
+      } else if (item.classList.contains("day-humid")) {
+        item.textContent = dayHumid + " %";
+      } else if (item.classList.contains("day-wind")) {
+        item.textContent = dayWind + " km/h";
+      } else if (item.classList.contains("day-icon")) {
+        item.innerHTML = dayIcon;
+      } else {
+        console.log(item);
+      }
+    });
+  }
+};
+
+addFutureWeatherData("Monday", "23", "78", "23", "");
 
 // retrieves background image based on given city
 const getBackgroundImage = async (city) => {
@@ -132,7 +156,7 @@ const geolocation = getGeolocation(city);
 geolocation.then((gData) => {
   const wData = getWeatherData(gData.lat, gData.lon);
   wData.then((dat) => {
-    addToDOM(
+    addTodayWeatherData(
       gData.name,
       gData.state,
       dat.temp,
